dc3cb4cf36f370195eed5439181e85175cbbcc11
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-24 01:09:16 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,170 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Plush Toy',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Soar to new heights with this Bald Eagle stuffed animal and show your patriotic pride.',
+            },
+            {
+                text: 'Makes a nice gift for kids and even adults, especially bird lovers, as customers admire the exquisite attention to detail of this Bald Eagle.',
+            },
+            {
+                text: 'At about 8 inches and tall and light weight allows this symbol of the United States of America to easily be brought where ever your heart desires.',
+            },
+            {
+                text: 'This realistic stuffed animal is created with high quality materials and is surface washable.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Multi-Purpose Scrub Brush',
+        price: 29.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Oscillating heads scrubs 60 times per second, so you don’t have to.',
+            },
+            {
+                text: 'Interchangeable accessory brushes give you the right tool for the job.',
+            },
+            {
+                text: 'Water resistant assembly for durability you expect.',
+            },
+            {
+                text: 'Pulse and Continuous scrubbing settings.',
+            },
+            {
+                text: 'Ergonomic grip and rubberized molding for comfortable cleaning.',
+            },
+            {
+                text: 'Pulse and continuous scrubbing settings.',
+            },
+            {
+                text: 'Tool delivers 6 Volts of Power.',
+            },
+            {
+                text: '4 AA batteries included.',
+            },
+        ],
+        images: ['./img/img3-1.jpg', './img/img3-2.png', './img/img3-3.jpg', './img/img3-4.jpg'],
+    },
+    {
+        id: 'product-3',
+        name: 'Shaving Strop',
+        price: 64.90,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                title: 'A sharp & good razor strop',
+                text: 'The aptitude of wet shaving starts with an impressive razor and your new-fangled razor sharpener. This leather sharpening strop will relieve you retain your blade sharp and robust for years to return. It also works as a kitchen knife strop. The hard brass is sewed up perfectly for long-lasting connectivity, best in school for a little leather strop. With extreme strength and effortlessness of use, this quality cowhide strop will last for thousands of shaves.',
+            },
+            {
+                title: 'Perfect gift',
+                text: 'Leather strops are the perfect gift for the modish man. This is a gift of looking decent, sensation good, and comfort make males feel strongly handsome with their sharp straight razor made possible by this stylish strop.',
+            },
+            {
+                title: 'Strop & shave',
+                text: 'Not ever again bear the painful anguish of a dull razor. Fair strop it with a leather strop and watch your razor\'s edge rise back to life.',
+            },
+            {
+                title: 'Your lifetime & money-back guarantee',
+                text: 'We are sure guaranteed you will love straight razor strops. Our guarantee promise is humble and honest. If you don’t like the product, we will refund or exchange it for one of our products. Our shaving soaps should keep thousands of gentlemen happy concerning astonishing handsome faces created for touching and made for touching and looking great.',
+            },
+            {
+                text: 'Extra wide & 2.5 mm thick.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Rectangle Metal Wall Mirror',
+        price: 98.90,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Metal trimmed mirror.',
+            },
+            {
+                text: 'Measures 19.5" x 27.5"H.',
+            },
+            {
+                text: 'Shelf on base extends 5".',
+            },
+            {
+                text: 'Finish: Distressed Black.',
+            },
+            {
+                text: 'Wall Mounted mirror - hardware not included',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Kids Solid Wood Table and 2 Chair Set',
+        price: 119.98,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Kids’ table for playing, eating, reading, and more.',
+            },
+            {
+                text: 'Perfect height and size for children.',
+            },
+            {
+                text: 'Includes square table and two matching chairs.',
+            },
+            {
+                text: 'Made of durable, natural solid wood.',
+            },
+            {
+                text: 'Dynamic use in playrooms, bedrooms, kitchens, living rooms, or outdoors.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Rectangular Kitchen Step Trash Can Recycler',
+        price: 148.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Dual compartments easily separate your recyclables from your household trash.',
+            },
+            {
+                text: 'Fingerprint resistant stainless-steel finish.',
+            },
+            {
+                text: 'Durable cantilever foot pedal mechanism.',
+            },
+            {
+                text: 'Soft close damper lid.',
+            },
+            {
+                text: 'Stay-open lid position option.',
+            },
+            {
+                text: 'Built in deodorizer compartment.',
+            },
+            {
+                text: 'Plastic bag fixer holds trash bags in place.',
+            },
+            {
+                text: 'Both inner liners removable for ease of emptying.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png', './img/img13-4.png', './img/img13-5.png'],
+    },
+]
